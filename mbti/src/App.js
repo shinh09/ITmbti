dda7b5673f6d0d3561ff5fcf7595053a46aa8a97
Park Shinhyung <23102009@seoultech.ac.kr>
2024-12-11 21:38:32 +0900

    11일차 신형 develp-start로

--- mbti/src/App.js
+++ mbti/src/App.js
@@ -3,16 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StartPage from "./pages/StartPage";
 import QuestionPage from "./pages/QuestionPage";
 import ResultPage from "./pages/ResultPage"; 
+import LoadingPage from "./pages/LoadingPage";
+import Header from "./layout/header";
+import Footer from "./layout/footer";
 
 
 function App() {
   return (
     <Router>
+      <Header/>
         <Routes>
           <Route path="/" element={<StartPage />} />
           <Route path="/question" element={<QuestionPage />} />
+          <Route path="/loading" element={<LoadingPage />} />
           <Route path="/result" element={<ResultPage />} />
         </Routes>
+      <Footer/>
     </Router>
   );
 }
